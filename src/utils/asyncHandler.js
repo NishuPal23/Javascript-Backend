507eb0284a710a614e379dce4f5c2e2aa5ba693b
NishuPal23 <palnishu916@gmail.com>
2024-02-24 05:16:59 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,30 @@
+//Approach 2
+const asyncHandler = (requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+//Approach 1
+/*export {asyncHandler}
+const asyncHandler = (fn)=> async(req,res,next)=>{
+    try{
+        await fn(req,res,next)
+    }
+    catch(error){
+        res.status(err.code ||500).json({
+            success:false,
+            message:err.message
+        })
+    }
+}*/
